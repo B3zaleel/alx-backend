a64cb5a48a3978097827ba3b9cb37042490b8c58
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-05-02 22:04:46 +0100

    :wrench: Fix issue with key or item of put method being None

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -11,6 +11,8 @@ class BasicCache(BaseCaching):
     def put(self, key, item):
         """Adds an item in the cache.
         """
+        if key is None or item is None:
+            return
         self.cache_data[key] = item
 
     def get(self, key):
