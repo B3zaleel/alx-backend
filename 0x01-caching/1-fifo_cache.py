614e85555fa7816a3b6af1b8c7458a323633d1c2
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-05-02 10:12:20 +0100

    :white_check_mark: Complete task 1

---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""Basic caching module.
+"""
+from collections import OrderedDict
+
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """Represents an object that allows storing and
+    retrieving items from a dictionary with a FIFO
+    removal mechanism when the limit is reached.
+    """
+    def __init__(self):
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """Adds an item in the cache.
+        """
+        if key is None or item is None:
+            return
+        self.cache_data[key] = item
+        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+            first_key, _ = self.cache_data.popitem(False)
+            print("DISCARD:", first_key)
+
+    def get(self, key):
+        """Retrieves an item by key.
+        """
+        return self.cache_data.get(key, None)
--- 0x01-caching/README.md
+++ 0x01-caching/README.md
@@ -14,7 +14,7 @@ This project contains tasks for learning to cache data.
     + Must return the value in `self.cache_data` linked to `key`.
     + If `key` is `None` or if the `key` doesn't exist in `self.cache_data`, return `None`.
 
-+ [ ] 1. **FIFO caching**<br/>[1-fifo_cache.py](1-fifo_cache.py) contains a Python class `FIFOCache` that inherits from [`BaseCaching`](base_caching.py) and is a caching system:
++ [x] 1. **FIFO caching**<br/>[1-fifo_cache.py](1-fifo_cache.py) contains a Python class `FIFOCache` that inherits from [`BaseCaching`](base_caching.py) and is a caching system:
   + You must use `self.cache_data` - dictionary from the parent class [`BaseCaching`](base_caching.py).
   + You can overload `def __init__(self):` but don't forget to call the parent init: `super().__init__()`.
   + `def put(self, key, item):`:
