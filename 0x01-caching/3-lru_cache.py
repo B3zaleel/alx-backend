4a2b413d8fdec846ff3fd4e4fbf46b90c2a7d3af
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-05-02 11:14:54 +0100

    :wrench: Refactor script

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -24,8 +24,8 @@ class LRUCache(BaseCaching):
             return
         if key not in self.cache_data:
             if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
-                last_key, _ = self.cache_data.popitem(True)
-                print("DISCARD:", last_key)
+                lru_key, _ = self.cache_data.popitem(True)
+                print("DISCARD:", lru_key)
             self.cache_data[key] = item
             self.cache_data.move_to_end(key, last=False)
         else:
