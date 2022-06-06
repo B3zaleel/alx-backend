3db7434c8387695e1f50e78937d8a3a4cffc02d6
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-06-06 16:31:19 +0100

    :sparkles: Complete task 6

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,20 @@
+#!/usr/bin/yarn dev
+import { createQueue } from 'kue';
+
+const queue = createQueue({name: 'push_notification_code'});
+
+const job = queue.create('user info', {
+  phoneNumber: '07045679939',
+  message: 'Account registered',
+}).save();
+
+job
+  .on('enqueue', () => {
+    console.log('Notification job created:', job.id);
+  })
+  .on('complete', () => {
+    console.log('Notification job completed');
+  })
+  .on('failed attempt', () => {
+    console.log('Notification job failed');
+  });
