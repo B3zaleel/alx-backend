2c9def1545522db20f26f216bf9b9b253b3c8fda
Bezaleel Olakunori <bezaleeloci@gmail.com>
2022-06-06 11:26:42 +0100

    :sparkles: Complete task 1

---
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -0,0 +1,11 @@
+import { createClient } from 'redis';
+
+const client = createClient();
+
+client.on('error', (err) => {
+  console.log('Redis client not connected to the server:', err.toString());
+});
+
+client.on('connect', () => {
+  console.log('Redis client connected to the server');
+});
